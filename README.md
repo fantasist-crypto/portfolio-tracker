3467268e8878af6e4a6cfcb4af5968724ad3634e
Fantasist <100333503+fantasist-crypto@users.noreply.github.com>
2023-02-04 18:45:52 +0800

    feat: add "shadcn/ui"

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # Portfolio Tracker
+
 This is a portfolio tracker app built using [Next.js](https://nextjs.org/) + [Turbopack](https://turbo.build/pack) + [Tailwindcss](https://tailwindcss.com).
 
 ## Installation
+
 ```shell
 pnpm i
 ```
 
 ## Development
+
 ```shell
 pnpm dev
 ```
 
 ## Build
+
 ```shell
 pnpm build
 ```
 
 ## LICENSE
+
 [Apache-2.0](https://opensource.org/licenses/Apache-2.0)
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import type { ReactNode } from 'react'
 
 import '@/styles/globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className="bg-white font-sans">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-white font-sans text-zinc-900 antialiased dark:bg-zinc-900 dark:text-zinc-50">
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
